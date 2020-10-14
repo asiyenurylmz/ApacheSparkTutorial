03904a5f171df469276a44f05ec2ff6144b2f2c1
Asiyenur Yılmaz <asiyenurylmz17@gmail.com>
2020-10-14 19:48:03 +0300

    worldcup analysis

---
+++ SparkWorldCupAnalysis/src/main/java/com/GroupPlayer.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.io.Serializable;
+
+public class GroupPlayer implements Serializable {
+
+	String playerName;
+	int matchCount;
+
+	public GroupPlayer(String playerName, int matchCount) {
+		super();
+		this.playerName = playerName;
+		this.matchCount = matchCount;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getMatchCount() {
+		return matchCount;
+	}
+
+	public void setMatchCount(int matchCount) {
+		this.matchCount = matchCount;
+	}
+
+}
